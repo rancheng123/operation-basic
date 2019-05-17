f66e80f3b989ea7e4933ac926592cb0bc5a36661
邓铃波 <ran.cheng@mydreamplus.com>
2019-05-17 15:49:34 +0800

    接口超时

--- watch/index.js
+++ watch/index.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
 var fs = require('fs');
+var watch = require('gulp-watch');
 
 var srcDir = '/Users/deo/WebstormProjects/workPlace/businessDir/operation-basic/';
 var distDir = '/Users/deo/WebstormProjects/workPlace/businessDir/operation/node_modules/@operation/basic/';
+
 
 
 //测试  start
@@ -26,6 +28,7 @@ var count= 0;
 for(var item of controlDirOrFile){
     walkDir(item,(curPath)=>{
         singleEmit({
+            event: 'change',
             filePath: curPath,
             srcDir: srcDir,
             distDir: distDir
@@ -48,25 +51,55 @@ controlDirOrFile = controlDirOrFile.map((ele,i)=>{
     return ele
 })
 
-var watcher =gulp.watch(controlDirOrFile);
+watch(controlDirOrFile,function(result){
+    singleEmit({
+        event: result.event,
+        filePath: result.history[0],
+        srcDir: srcDir,
+        distDir: distDir
+    });
+})
+
+/*var watcher =gulp.watch(controlDirOrFile);
 watcher.on('change', function(filePath) {
     singleEmit({
         filePath: filePath,
         srcDir: srcDir,
         distDir: distDir
-    })
-});
+    });
+    console.log(filePath + ' 替换成功')
+
+});*/
 //监控所有文件  end
 
 
 
 //替换文件
 function singleEmit(opts){
-    var arr = opts.filePath.replace(opts.srcDir,'').split('/');
-    arr.pop();
+    if(
+        opts.event == 'add' ||
+        opts.event == 'change'
+    ){
 
-    gulp.src(opts.filePath)
-        .pipe(gulp.dest( opts.distDir + arr.join('/') ))
+
+        var arr = opts.filePath.replace(opts.srcDir,'').split('/');
+        arr.pop();
+
+
+        gulp.src(opts.filePath)
+            .pipe(gulp.dest( opts.distDir + arr.join('/') ))
+
+        console.log(opts.distDir + opts.filePath.replace(opts.srcDir,'') + ' 替换成功')
+
+
+    }else if(opts.event == 'unlink'){
+        fs.unlinkSync( opts.distDir + opts.filePath.replace(opts.srcDir,'') );
+
+        console.log(opts.distDir + opts.filePath.replace(opts.srcDir,'') + ' 删除成功')
+    }
+
+
+
 }
 
 
